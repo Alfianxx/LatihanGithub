ec9ba62405a9088498e0ecaff12e8f99d7fc14ca
Alfian <alfianhacker@gmail.com>
2021-06-19 17:53:17 +0800

    helo all

--- app/src/main/java/com/alfian/latihangit/MainActivity.kt
+++ app/src/main/java/com/alfian/latihangit/MainActivity.kt
@@ -2,6 +2,7 @@ package com.alfian.latihangit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
@@ -17,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        Snackbar.make(vi, "Text baru", Snackbar.LENGTH_SHORT).show()
+        Log.d("aaa", "onCreate: abc")
 
         Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show()
 
